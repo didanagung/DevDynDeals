8b32d4d41e884ec023e2016ea3970e5d30632ce8
didanagung <familytidai@gmail.com>
2024-10-13 11:19:53 +0700

    initial backend

---
+++ backend/app/Models/Products.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Products extends Model
+{
+    // use HasFactory;
+    // use HasUuids;
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'statusenabled', 'name', 'qty_product', 'short_description', 'description'
+    ];
+
+    // membuat uuid secara otomatis
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+}
